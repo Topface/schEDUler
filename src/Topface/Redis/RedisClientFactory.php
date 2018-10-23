fb918eecca7543b1898e9053b5f0e02c13bc9003
Богдевич Глеб <g.bogdevich@topface.com>
2018-10-23 19:02:32 +0300

    Fix redis factory interface realisation

--- src/Topface/Redis/RedisClientFactory.php
+++ src/Topface/Redis/RedisClientFactory.php
@@ -10,12 +10,24 @@ use Scheduler\SchedulerRedisClientFactoryInterface;
  * @task
  */
 class RedisClientFactory implements SchedulerRedisClientFactoryInterface {
-
     /**
+     * Получаем редис для расписания задач
+     *
      * @return Client
      */
     public function getRedisClient(): Client {
         $Client = new Client();
+        return $Client;
+    }
+
+    /**
+     * Получаем редис для очередей разборщиков
+     *
+     * @return Client
+     */
+    public function getRedisQueueClient(): Client {
+        $Client = new Client();
+
         return $Client;
     }
 }
