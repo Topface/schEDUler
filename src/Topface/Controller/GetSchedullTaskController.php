72d6c9a7a983e988f376d63123cb0567698f83dc
Andrey Mostovoy <stalk.4.me@gmail.com>
2018-10-23 18:59:42 +0300

    controller 3

--- src/Topface/Controller/GetSchedullTaskController.php
+++ src/Topface/Controller/GetSchedullTaskController.php
@@ -2,13 +2,28 @@
 
 namespace Topface\Controller;
 
+use Scheduler\SchedulerQueueWorker\SchedulerQueueWorkerInterface;
+
 /**
  * @author Andrey Mostovoy
  * @task
  */
 class GetSchedullTaskController implements GetSchedullTaskControllerInterface {
+    /** @var SchedulerQueueWorkerInterface */
+    private $SchedulerQueueWorker;
 
+    /**
+     * GetSchedullTaskController constructor.
+     * @param SchedulerQueueWorkerInterface $SchedulerQueueWorker
+     */
+    public function __construct(SchedulerQueueWorkerInterface $SchedulerQueueWorker) {
+        $this->SchedulerQueueWorker = $SchedulerQueueWorker;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function start() {
-        // TODO: Implement start() method.
+        $this->SchedulerQueueWorker->publishCurrentTasks();
     }
 }
--- src/Topface/Controller/HandleSchedullTaskController.php
+++ src/Topface/Controller/HandleSchedullTaskController.php
@@ -2,13 +2,28 @@
 
 namespace Topface\Controller;
 
+use Scheduler\HandlerWorker\HandlerWorkerInterface;
+
 /**
  * @author Andrey Mostovoy
  * @task
  */
 class HandleSchedullTaskController implements HandleSchedullTaskControllerInterface {
+    /** @var HandlerWorkerInterface  */
+    private $HandlerWorker;
 
+    /**
+     * HandleSchedullTaskController constructor.
+     * @param HandlerWorkerInterface $HandlerWorker
+     */
+    public function __construct(HandlerWorkerInterface $HandlerWorker) {
+        $this->HandlerWorker = $HandlerWorker;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function start() {
-        // TODO: Implement start() method.
+        $this->HandlerWorker->run();
     }
 }
